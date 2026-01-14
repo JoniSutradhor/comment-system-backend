0c6a9c5302a1b4e260a6845d3b6a7f9fe94e158f
J.K. Sutradhor <jksutradhor@gmail.com>
2026-01-14 22:20:22 +0600

    Vercel permission added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173' || 'https://comment-system-frontend-five.vercel.app',
     methods: ['GET', 'POST']
   }
 });
